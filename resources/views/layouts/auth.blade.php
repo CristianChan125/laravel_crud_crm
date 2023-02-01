ff22d54a2de668ac440e75b2f167215821872e3a
Chan125 <contacto@chancorp.com>
2023-01-31 22:05:09 -0600

    first commit

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Log in</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="{{ asset('css/plugins/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/adminlte.min.css') }}">
+
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  @yield('content')
+</div>
+<!-- /.login-box -->
+
+<script src="{{ asset('js/plugins/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/bootstrap.bundle.js') }}"></script>
+    <script src="{{ asset('js/adminlte.min.js') }}"></script>
+</body>
+</html>
